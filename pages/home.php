2e7cdde6168fc8b8bfdbac4a223e482134e4f4ae
disadb ** <disadb@gmail.com>
2015-11-05 13:09:33 +0800

    Replace global settings with Settings class use.

--- pages/home.php
+++ pages/home.php
@@ -10,69 +10,37 @@ $page = Page::getInstance();
 
 //======================================================================================================//
 //============================================= MAIN ===================================================//
+$settings = Settings::get();
+$tpl = new Template();
+$tpl->set_file("$page->page-page", "backstage/templates/$page->page.html");
+$tpl->set_block("$page->page-page", 'latestArticlesBlock', 'theLatestArticlesBlock');
+$tpl->set_block('latestArticlesBlock', 'articleBlock', 'theArticleBlock');
+$tpl->set_block("$page->page-page", 'latestArticlesBlockImages', 'theLatestArticlesBlockImages');
+$tpl->set_block('latestArticlesBlockImages', 'articleBlockImages', 'theArticleBlockImages');
+$tpl->set_var('ROOT', $settings->root);
+
+handleAjax(function()
+{
+	$articles = getArticles(10);
+	return ["html" => renderArticles($articles)];
+});
+
+
+
+
 // Disable breadcrumbs on home page only.
-$page = Page::getInstance();
 $page->setBreadcrumbsVisiblity(false);
 
 // Disable h1 title on home page only.
 $page->setH1(null);
 
-$tpl = new Template('.');
-$tpl->set_file("$page->page-page", "backstage/templates/$page->page.html");
-$tpl->set_block("$page->page-page", 'latestArticleBlock', 'theLatestArticleBlock');
-$tpl->set_block('latestArticleBlock', 'articleBlock', 'theArticleBlock');
-
-// Retrieve published articles from DB.
-$db = database::getInstance();
-$q = $db->query();
-$q->select('articles',
-		   [$q->col("content_$language"),
-		    $q->col("page")->as('page'),
-		    $q->col("title_$language")->as('title'),
-		    $q->col('created'),
-		    $q->concat($q->colIn('firstName', 'users'), ' ', $q->colIn('lastName', 'users'))->as('author')])
-  ->relate('articles.author', 'users.id')
-  ->relate('articles.id', 'pages.article')
-  ->relate('articles.category', 'article_categories.id');
-$w = $q->where();
-$w->col('published')->eq(1);//->and($w->colIn('name', 'article_categories')->eq('travel'));
-
-$articles = $q->run()
-              ->loadObjects();
-
-if (count($articles))
-{
-	foreach ($articles as $k => $article)
-	{
-		$created = new DateTime($article->created);
-		$tpl->set_var(['articleLink' => url($article->page),
-					   'articleTitle' => $article->title,
-					   'publishedByOn' => text(21,
-					   					[
-					   					    'contexts' => 'article',
-					   						'formats' =>
-					   						[
-					   							'sprintf' =>
-					   							[
-			   										$article->author,
-												  	$created->format($language == 'fr' ? 'd/m/Y' : 'Y-m-d'),
-												 	$created->format($language == 'fr' ? 'H\hi' : 'H:i')
-												]
-											]
-										])]);
-		$tpl->parse('theArticleBlock', 'articleBlock', true);
-	}
-
-	$tpl->set_var('latestArticlesTitle', text(44));
-	$tpl->parse('theLatestArticleBlock', 'latestArticleBlock', true);
-}
-else $tpl->set_var('theLatestArticles', '');
-
-$tpl->set_var('ROOT', $settings->root);
+$articles = getArticles();
+renderArticles($articles);
 
 $content = $tpl->parse('display', "$page->page-page");
 //============================================ end of MAIN =============================================//
 //======================================================================================================//
+
 /**
  * Render the articles list.
  *
@@ -81,7 +49,7 @@ $content = $tpl->parse('display', "$page->page-page");
  */
 function renderArticles($articles)
 {
-	global $tpl, $latestArticlesUsePictures;
+	global $tpl;
 	$settings = Settings::get();
 	$language = Language::getCurrent();
 	$latestArticlesUsePictures = $settings->latestArticlesUsePictures;
