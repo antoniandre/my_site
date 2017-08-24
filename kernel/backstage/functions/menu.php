04ae9d95b8c7ea32662331e1433ae0c72b3df25d
disadb ** <disadb@gmail.com>
2017-08-24 15:57:11 +1000

    Add a menu function ready to override.

---
+++ kernel/backstage/functions/menu.php
@@ -0,0 +1,12 @@
+<?php
+/* Define your main menu here. E.g.
+    return doMenu
+    (
+        ['page1', 'page2',...],
+        ['class' => 'main-menu', 'showIcons' => true]
+    );
+ */
+
+return null;
+
+?>
